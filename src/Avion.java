2e162711d604ad88a301d13af52cd9d0de505f43
Jonathan <l22390450@chetumal.tecnm.mx>
2023-06-18 23:07:29 -0500

    1

--- src/Avion.java
+++ src/Avion.java
@@ -1,3 +1,11 @@
 public class Avion {
+    private String modelo;
 
+    public Avion(String modelo){
+        this.modelo= modelo;
+    }
+
+    public String getModelo(){
+        return modelo;
+    }
 }
--- src/Itinerario.java
+++ src/Itinerario.java
@@ -1,3 +1,10 @@
 public class Itinerario {
+    private String ciudadOrigen;
+    private String destino;
+    private String horaInicio;
+    private String horaFin;
+    private Avion avion;
+    private Piloto piloto;
+
 
 }
